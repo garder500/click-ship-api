a5c0f5e8dd553c759ddb34b869c2678d8e2366c0
garder500 <jeremy27.clara22@gmail.com>
2025-02-16 15:48:54 +0100

    Ajout de la journalisation dans la configuration de GORM et utilisation d'une clé de contexte pour la gestion de la base de données

--- models/database.go
+++ models/database.go
@@ -6,17 +6,24 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func InitDatabase() (*gorm.DB, error) {
 	dsn := "host=127.0.0.1 user=clickship password=clickship dbname=clickship port=5432 sslmode=disable TimeZone=Europe/Paris"
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 }
 
+type contextKey string
+
+const databaseKey contextKey = "database"
+
 func ContextWithDatabase(ctx context.Context, db *gorm.DB) context.Context {
-	return context.WithValue(ctx, "database", db)
+	return context.WithValue(ctx, databaseKey, db)
 }
 
 func GetDatabase(r *http.Request) *gorm.DB {
-	return r.Context().Value("database").(*gorm.DB)
+	return r.Context().Value(databaseKey).(*gorm.DB)
 }
